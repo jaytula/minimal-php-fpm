7473d013185d995871b6aaf9175ad91b75b2be96
Jay T <jaytula@gmail.com>
2020-04-27 00:02:13 -0700

    Working example with syslog
    openlog with LOG_PERROR is the key

--- fpm/www/index.php
+++ fpm/www/index.php
@@ -2,5 +2,7 @@
 
 echo phpinfo();
 
-$arr = array('aaa' => 'bbb');
-error_log(var_export($arr, true));
+// $arr = array('aaa' => 'bbb');
+// error_log(var_export($arr, true));
+openlog('heya', LOG_NDELAY | LOG_PERROR, LOG_LOCAL0);
+syslog(LOG_ERR, 'whatever');
